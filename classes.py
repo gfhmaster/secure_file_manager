59cccd103d36a5ca2b08fe44377c2ebc9adb4f41
gfhmaster <iliarich@ya.ru>
2025-10-04 15:28:13 +0300

    Add GitHub Actions workflow for CI/CD

--- classes.py
+++ classes.py
@@ -94,34 +94,3 @@ class FileManager:
     def list_all_files(self):
         return [f.filename for f in self._all_files]
 
-# -------------------------------
-# Пример использования
-# -------------------------------
-# Создание пользователей
-alice = User("Alice", "hash1")
-bob = User("Bob", "hash2")
-
-# Создание файлов
-file1 = File("document.txt", alice)
-file2 = EncryptedFile("secret.txt", alice)
-file3 = ArchiveFile("backup.zip", bob)
-
-# Работа с файлами
-alice.upload_file(file1)
-alice.upload_file(file2)
-file2.encrypt()
-bob.upload_file(file3)
-
-# FileManager управляет всеми файлами
-manager = FileManager()
-manager.add_file(file1)
-manager.add_file(file2)
-manager.add_file(file3)
-
-# Просмотр информации
-for f in manager._all_files:
-    print(f.info())
-
-# Совместный доступ
-alice.share_file(file2, bob)
-print("Файлы Bob:", bob.list_files())
