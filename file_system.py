b4330f44b9360a4a423e66419cdcc1fdc0355c6e
gfhmaster <iliarich@ya.ru>
2025-10-04 16:07:53 +0300

    ПЗ № 18

--- file_system.py
+++ file_system.py
@@ -9,7 +9,7 @@ class User:
     Инкапсуляция: пароль и список файлов защищены от прямого доступа.
     """
     def __init__(self, username, password_hash):
-        self.username = username
+        self.name = username
         self._password_hash = password_hash  # защищенный атрибут
         self._files = []  # список файлов пользователя, инкапсулирован
 
